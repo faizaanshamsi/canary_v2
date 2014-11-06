eb649840c4d6babc460c39bef0936a3003f709d0
faizaanshamsi <faizaan2000@gmail.com>
2014-11-06 15:30:55 -0500

    fixes #23

--- spec/features/seller_adds_an_item_spec.rb
+++ spec/features/seller_adds_an_item_spec.rb
@@ -2,26 +2,21 @@ require 'rails_helper'
 
 feature 'seller adds item' do
   scenario 'seller adds a single item with multiple images' do
-    seller = FactoryGirl.create(:user)
-    item_attrs = { title: 'Excellent oak table',
-                   description: 'This excellent oak table is the best table.',
-                   dimensions: '10 x 10',
-                   brand: 'Amish',
-                   condition: 'excellent',
-                   age: 20 } 
+    item = FactoryGirl.build(:item)
+    seller = item.user
+    sign_in_as(seller)
 
-   sign_in_as(seller)
+    visit new_item_path
+# comment for test issue 23
+    fill_in "Title", with: item.title
+    fill_in "Description", with: item.description
+    fill_in "Dimensions", with: item.dimensions
+    fill_in "Brand", with: item.brand
+    select "Excellent", from: item.condition
+    fill_in "Age in years", with: item.age
 
-   visit new_item_path
-
-    fill_in "Title", with: item_attrs[:title]
-    fill_in "Description", with: item_attrs[:description]
-    fill_in "Dimensions", with: item_attrs[:dimensions]
-    fill_in "Brand", with: item_attrs[:brand]
-    select "Excellent", from: "Condition"
-    fill_in "Age in years", with: item_attrs[:age]
-
-    select  "Dining table", from: "Category"
+    select  "Dining Room", from: "Room"
+    select  "Dining Table", from: "Category"
     click_on "Continue"
 
     expect(page).to have_content("Item successfully created")
@@ -31,8 +26,8 @@ feature 'seller adds item' do
     attach_file "Add photo", Rails.root.join('spec/fixtures/table.jpeg')
     click_on "Add another photo"
     attach_file "Add photo", Rails.root.join('spec/fixtures/table_2.jpeg')
-    
+
     expect(page).to have_content("Upload successful") 
   end
-  
+
 end
